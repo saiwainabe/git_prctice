238c05bc641da7bd1183e1b2c4a99cf35478a51d
saiwainabe <94275066+saiwainabe@users.noreply.github.com>
2021-11-14 13:54:16 +0900

    1commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// ｈ１要素を取得
+const h2 = document.querySelector('h2');
+
+h2.addEventListener('click', () => {
+    h2.textContent = 'Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!';
+});
